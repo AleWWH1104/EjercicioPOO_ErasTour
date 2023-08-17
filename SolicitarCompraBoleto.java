199f433320ad48598a0f920f7dd5d0219dd1cb17
Iris <irisale.ayala@gmail.com>
2023-08-17 17:35:44 -0600

    Creacion de la funcion para solicitar la informacion

--- SolicitarCompraBoleto.java
+++ SolicitarCompraBoleto.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class SolicitarCompraBoleto {
     //Declaracion de variables 
-    private String nombre;
-    private String email;
-    private Integer cant_boletos;
-    private Double presupuesto;
+    public String nombre;
+    public String email;
+    public Integer cant_boletos;
+    public Double presupuesto;
 
-
+    public void informacion(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingrese su nombre");
+		nombre = in.nextLine();
+		System.out.println("Ingrese su email");
+		email = in.nextLine();
+        System.out.println("Ingrese la cantidad de boletos a comprar");
+		cant_boletos = in.nextInt();
+        System.out.println("Ingrese su presupuesto");
+		presupuesto = in.nextDouble();
+    }
 }
