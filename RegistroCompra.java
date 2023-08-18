6fff021719feb37196b47c18c88a05d1fff12969
Iris <irisale.ayala@gmail.com>
2023-08-18 07:29:51 -0600

    Creacion de la clase con sus funciones para el registro de los compradores

---
+++ RegistroCompra.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class RegistroCompra {
+    //Lista que contiene mis compradores de boletos
+    public ArrayList<Comprador> Compradores;
+
+    public boolean GuardarComprador(Comprador persona) {
+        int cantidadInicial = Compradores.size();
+        Compradores.add(persona);
+        return cantidadInicial < Compradores.size();
+    }
+
+    //Mostrar el registro de compradores
+    public ArrayList<Comprador> mostrarRegistro() {
+        return Compradores;
+    }
+
+
+}
