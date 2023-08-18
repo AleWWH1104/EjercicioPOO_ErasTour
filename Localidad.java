7a8c538be0a1d28ed3ec89efe61cc0171ac30922
Iris <irisale.ayala@gmail.com>
2023-08-17 18:49:58 -0600

    Creacion de la funcion para seleccionar la localidad de manera aleatoria

--- Localidad.java
+++ Localidad.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Localidad {
+    //Declarar los espacios de cada localidad
     public Integer Localidad1 = 20;
     public Integer Localidad5 = 20;
     public Integer Localidad10 = 20;
+
+    public void localidadAleatoria(){
+        ArrayList<String> localidadE = new ArrayList<String>();
+        //Agregar las opciones de localidad al Arraylist
+        localidadE.add("Localidad 1");
+        localidadE.add("Localidad 5");
+        localidadE.add("Localidad 10");
+
+        // Crear una instancia de Random
+        Random random = new Random();
+        // Generar un índice aleatorio del ArrayList
+        int indiceAleatorio = random.nextInt(localidadE.size());
+        // Acceder al elemento aleatorio e imprimirlo
+        String LocalidadSeleccionada = localidadE.get(indiceAleatorio);
+        System.out.println("Localidad seleccionada aleatoriamente: " + LocalidadSeleccionada);
+    
+    }
+
+    
+
 }
