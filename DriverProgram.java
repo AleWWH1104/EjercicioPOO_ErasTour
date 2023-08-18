47e62b62515b1fed9b9144bfa7b775a3cf324d19
Iris <irisale.ayala@gmail.com>
2023-08-18 08:01:32 -0600

    Creacion de la clase driverProgram y se hizo la funcion que muestra el menu de entrada

---
+++ DriverProgram.java
@@ -0,0 +1,14 @@
+public class DriverProgram {
+    
+    public void MostrarMenu(){
+        System.out.println("Bienvenidx a la compra de Boletos de Eras Tour");
+        System.out.println("Seleccione una opcion");
+        System.out.println("1:Nuevo Comprador");
+        System.out.println("2:Nueva solicitud de boletos");
+        System.out.println("3:Consultar disponibilidad total");
+        System.out.println("4:Consultar por localidad");
+        System.out.println("5:Reporte de caja");
+        System.out.println("6:Codigo especial");
+        System.out.println("7:Salir del programa");
+    }
+}
