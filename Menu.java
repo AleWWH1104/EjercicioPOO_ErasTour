62377246922642373fe6d8ae13ceec93b469b373
David Dominguez <dominguezdavid396@gmail.com>
2023-08-18 19:32:13 -0600

    Pruebas en menu

--- Menu.java
+++ Menu.java
@@ -1,17 +1,50 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
-public class Menu{
-    public static void main(String[] args) {
-        //SolicitarCompraBoleto solicitarBoleto = new SolicitarCompraBoleto();
-        Localidad localidad = new Localidad();
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Bienvenidx al sorteo de boletos para Eras Tour");
-        System.out.println("Deseas hacer una compra normal o compra especial?: ");
-
+public class Menu {
+    public void mostrarMenu(Localidad localidad, Scanner in) {
+        System.out.println("Bienvenido a la compra de boletos para Eras Tour");
         
+        while (true) {
+            System.out.println("\nSeleccione una opción:");
+            System.out.println("1: Compra normal");
+            System.out.println("2: Compra especial");
+            System.out.println("3: Salir");
+
+            int opcion = in.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    realizarCompraNormal(localidad, in);
+                    break;
+                case 2:
+                    realizarCompraEspecial(localidad, in);
+                    break;
+                case 3:
+                    System.out.println("¡Gracias por usar el sistema de boletos!");
+                    return; // Salir del programa
+                default:
+                    System.out.println("Opción inválida. Por favor, elija una opción válida.");
+            }
+        }
+    }
+
+    private void realizarCompraNormal(Localidad localidad, Scanner in) {
+        Comprador comprador = new Comprador();
+        comprador.informacion();
+
+        // Implementar la lógica de compra normal aquí usando el objeto 'localidad' y 'comprador'
+    }
+
+    private void realizarCompraEspecial(Localidad localidad, Scanner in) {
+        System.out.println("Ingrese el código especial:");
+        int codigoEspecial = in.nextInt();
+
+        CodigoEspecial codigo = new CodigoEspecial(codigoEspecial);
+
+        if (codigo.getCodigo() != 0) {
+            System.out.println("¡Código especial válido!");
+
+            // Implementar compra especial
+
     }
 }
-=======
->>>>>>> 1db623092cf83736dc7923589e7bdc459c83cdd3
