cf570376c347c290df15279ea5ad837225899c60
Iris <irisale.ayala@gmail.com>
2023-08-18 21:59:47 -0600

    Agregar todas las funciones al menu

--- Menu.java
+++ Menu.java
@@ -1,26 +1,50 @@
 import java.util.Scanner;
 
 public class Menu {
+    //Localidad localidad = new Localidad();
     public void mostrarMenu(Localidad localidad, Scanner in) {
         System.out.println("Bienvenido a la compra de boletos para Eras Tour");
         
         boolean salir = false;
         while (!salir) {
             System.out.println("\nSeleccione una opción:");
-            System.out.println("1: Compra normal");
-            System.out.println("2: Compra especial");
-            System.out.println("3: Salir");
-
+            System.out.println("1: Nuevo Comprador");
+            System.out.println("2: Nueva solicitud de boletos");
+            System.out.println("3: Consultar Disponibilidad total");
+            System.out.println("4: Consultar Disponibilidad individual");
+            System.out.println("5: Reporte de caja");
+            System.out.println("6: Codigo especial");
+            System.out.println("7: Salir");
             int opcion = in.nextInt();
 
             switch (opcion) {
+                
                 case 1:
-                    realizarCompraNormal(localidad, in);
+                    Comprador comprador = new Comprador();
+                    comprador.informacion();
                     break;
                 case 2:
-                    realizarCompraEspecial(localidad, in);
+                    realizarCompraNormal(localidad, in);
                     break;
                 case 3:
+                    int espaciosDisponiblesTotal = localidad.obtenerEspaciosDisponibles("Localidad 1") +
+                    localidad.obtenerEspaciosDisponibles("Localidad 5") +
+                    localidad.obtenerEspaciosDisponibles("Localidad 10");
+                    System.out.println("La disponibilidad total de boletos es: " + espaciosDisponiblesTotal);
+                    break;
+                case 4:
+                    System.out.println("Espacios disponibles localidad 1: "+ localidad.obtenerEspaciosDisponibles("Localidad 1"));
+                    System.out.println("Espacios disponibles localidad 5: "+ localidad.obtenerEspaciosDisponibles("Localidad 5"));
+                    System.out.println("Espacios disponibles localidad 10: "+ localidad.obtenerEspaciosDisponibles("Localidad 10"));
+                    break;
+                case 5:
+                    System.out.println("Ingresos generados: $" + localidad.obtenerIngresosGenerados());
+                    
+                    break;
+                case 6:
+                    realizarCompraEspecial(localidad, in);
+                    break;
+                case 7:
                     System.out.println("¡Gracias por usar el sistema de boletos!");
                     salir = true;
                     break; // Salir del programa
@@ -32,8 +56,7 @@ public class Menu {
 
     private void realizarCompraNormal(Localidad localidad, Scanner in) {
         Comprador comprador = new Comprador();
-        comprador.informacion();
-    
+        
         // Verificar si el ticket es válido para la compra de boletos
         if (!comprador.getTicket().isValidForPurchase()) {
             System.out.println("Lo sentimos, el ticket no es valido para esta compra.");
