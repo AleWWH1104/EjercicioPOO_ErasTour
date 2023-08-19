5b92393a4f736a2da54af57c02a6b773f5ddd3d2
Iris <irisale.ayala@gmail.com>
2023-08-18 21:03:02 -0600

    Prueba para actualizar

--- Comprador.java
+++ Comprador.java
@@ -6,14 +6,8 @@ public class Comprador {
     private Integer cant_boletos;
     private Double presupuesto;
 
-<<<<<<< HEAD
     // variable para ticket
     private Ticket ticket;
-=======
-    private Ticket ticket;
-    
-
->>>>>>> 44491cb40c621cfbc15d5d1b4fd9321ee6f25133
     public Comprador() {
         ticket = new Ticket();
     }
