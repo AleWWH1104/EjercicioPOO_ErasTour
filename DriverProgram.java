541a212efe25d66fc86eeb3828d20fa97112f1da
Iris <irisale.ayala@gmail.com>
2023-08-18 22:00:28 -0600

    Correccion para mostrar el menu

--- DriverProgram.java
+++ DriverProgram.java
@@ -5,20 +5,6 @@ public class DriverProgram {
         Scanner in = new Scanner(System.in);
         Localidad localidad = new Localidad();
         Menu menu = new Menu();
-
-        //System.out.println("Menu");
-
-        // con ayuda de chat
-        // al usar el case 3, para salie no sale del ciclo del programa, como lo puedo corregir?
-        boolean salir = false;
-        while (!salir) {
-            menu.mostrarMenu(localidad, in);
-            System.out.println("¿Desea salir del programa? (si/no):");
-            String respuesta = in.next();
-            if (respuesta.equalsIgnoreCase("si")) {
-                salir = true;
-                System.out.println("¡Gracias por usar el sistema de boletos!");
-            }
-        }
+        menu.mostrarMenu(localidad, in);
     }
 }
